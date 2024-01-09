5e06688a9113c3246b7cb2ce0ff5aeba854db5bd
Jose Gaston <jmcbgaston@gmail.com>
2024-01-09 14:57:34 -0800

    add Stack functionality

--- stack.js
+++ stack.js
@@ -1,22 +1,33 @@
 class Stack {
+  // can accept any number of args
   constructor(...elements) {
+    // instantiate stack as an array of the elements
     this.stack = [...elements];
   }
 
-  push() {
+  push(...items) {
     // add item to the top of the stack
+    this.stack.push(...items);
   }
 
-  pop() {
-    // remove the item at the top of the stack
+  pop(count = 1) {
+    // remove n items off the top of the stack
+    let countdown = count;
+
+    while (countdown > 0) {
+      this.stack.pop();
+      countdown--;
+    }
   }
 
   peek() {
     // what is the item at the top of the stack?
+    return this.stack[this.stack.length - 1];
   }
 
   empty() {
     // is the stack empty
+    return this.stack.legth === 0;
   }
 
   search() {
@@ -25,10 +36,15 @@ class Stack {
 }
 
 let stack = new Stack("a", "b", "c");
-console.log(stack);
+console.log({ init: stack });
 
-// stack.push(1);
-// console.log(stack);
+stack.push(1, 2, 3);
+console.log({ push: stack });
 
-// stack.pop();
-// console.log(stack);
+stack.pop(1);
+console.log({ pop: stack });
+
+const peekStack = stack.peek();
+console.log({ peek: peekStack });
+
+// How to execute: `node stack.js`
